95ea6d99fe3276086a32bc55d90c1c2edb62f9e0
chompDev <27521899+chompDev@users.noreply.github.com>
2025-07-19 17:49:16 +0000

    .NET Format Style Fixes

--- SPTarkov.Server/Program.cs
+++ SPTarkov.Server/Program.cs
@@ -123,7 +123,9 @@ public static class Program
             {
                 // This method is not expected to be async so we need to wait for the Task instead of using await keyword
                 options.ApplicationServices.GetService<OnWebAppBuildModLoader>()!.OnLoad().Wait();
-                var httpConfig = options.ApplicationServices.GetService<ConfigServer>()?.GetConfig<HttpConfig>()!;
+                var httpConfig = options
+                    .ApplicationServices.GetService<ConfigServer>()
+                    ?.GetConfig<HttpConfig>()!;
                 var certHelper = options.ApplicationServices.GetService<CertificateHelper>()!;
                 options.Listen(
                     IPAddress.Parse(httpConfig.Ip),
